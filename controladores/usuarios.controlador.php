c1d00ce1c3887290bc4a3e00f795c0a168f6a186
TonyNavas <tonynavas777@gmail.com>
2021-01-24 23:53:56 -0600

    Agregue la ruta donde se guardaran las fotos del usuario

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -47,71 +47,133 @@ class ControladorUsuarios{
 	{
 		if (isset($_POST["nuevoUsuario"])){
 
-			if (preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevoNombre"]) &&
-				preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoUsuario"])             &&
-				preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoPassword"]))
-				{
-					$tabla = "usuarios";
+	if (preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevoNombre"]) &&
+		preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoUsuario"])             &&
+		preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoPassword"])){
 
-					$datos = array("nombre" => $_POST["nuevoNombre"],
-								   "usuario" => $_POST["nuevoUsuario"],
-								   "password" => $_POST["nuevoPassword"], 
-								   "perfil" => $_POST["nuevoPerfil"],);
-					
-					$respuesta = ModeloUsuarios::mdlIngresarUsuario($tabla, $datos);
+		/*=============================================
+		VALIDAR IMAGEN
+		=============================================*/
 
-					if ($respuesta == "ok")
-					{
-						echo '<script>
-					
-					swal({
+		$ruta ="";
 
-						type: "success",
-						title: "¡El usuario ha sido guardado correctamente!",
-						showConfirmButton: true,
-						confirmButtonText: "Cerrar"
+		if(isset($_FILES["nuevaFoto"]["tmp_name"])){
 
-					}).then(function(result){
+		list($ancho, $alto) = getimagesize($_FILES["nuevaFoto"]["tmp_name"]);
 
-						if(result.value){
-						
-							window.location = "usuarios";
+		$nuevoAncho = 500;
 
-						}
+		$nuevoAlto = 500;
 
-					});
+			/*=============================================
+			CREAMOS EL DIRECTORIO DONDE VAMOS A GUARDAR LA FOTO DEL USUARIO
+			=============================================*/
 
-					</script>';
-					}
-				}
-				else
-				{
-					echo '<script>
-					
-					swal({
+			$directorio = "vistas/img/usuarios/".$_POST["nuevoUsuario"];
 
-						type: "error",
-						title: "¡El usuario no puede ir vacío o llevar caracteres especiales!",
-						showConfirmButton: true,
-						confirmButtonText: "Cerrar"
+			mkdir($directorio, 0755);
 
-					}).then(function(result){
+			/*=============================================
+			DE ACUERDO AL TIPO DE IMAGEN APLICAMOS LAS FUNCIONES POR DEFECTO DE PHP
+			=============================================*/
 
-						if(result.value){
-						
-							window.location = "usuarios";
+			if($_FILES["nuevaFoto"]["type"] == "image/jpeg"){
 
-						}
+				/*=============================================
+				GUARDAMOS LA IMAGEN EN EL DIRECTORIO
+				=============================================*/
 
-					});
+				$aleatorio = mt_rand(100,999);
 
-					</script>';
-				}
+				$ruta = "vistas/img/usuarios/".$_POST["nuevoUsuario"]."/".$aleatorio.".jpg";
 
+				$origen = imagecreatefromjpeg($_FILES["nuevaFoto"]["tmp_name"]);						
+
+				$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+				imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+				imagejpeg($destino, $ruta);
 		}
+		if($_FILES["nuevaFoto"]["type"] == "image/png"){
+
+			/*=============================================
+			GUARDAMOS LA IMAGEN EN EL DIRECTORIO
+			=============================================*/
+
+			$aleatorio = mt_rand(100,999);
+
+			$ruta = "vistas/img/usuarios/".$_POST["nuevoUsuario"]."/".$aleatorio.".png";
+
+			$origen = imagecreatefrompng($_FILES["nuevaFoto"]["tmp_name"]);						
+
+			$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+			imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+			imagepng($destino, $ruta);
 	}
 
+	}
+			$tabla = "usuarios";
 
+			$datos = array("nombre" => $_POST["nuevoNombre"],
+						   "usuario" => $_POST["nuevoUsuario"],
+						   "password" => $_POST["nuevoPassword"], 
+						   "perfil" => $_POST["nuevoPerfil"],
+							"foto" =>$ruta);
+			
+			$respuesta = ModeloUsuarios::mdlIngresarUsuario($tabla, $datos);
+
+			if ($respuesta == "ok")
+			{
+				echo '<script>
+			
+			swal({
+
+				type: "success",
+				title: "¡El usuario ha sido guardado correctamente!",
+				showConfirmButton: true,
+				confirmButtonText: "Cerrar"
+
+			}).then(function(result){
+
+				if(result.value){
+				
+					window.location = "usuarios";
+
+				}
+
+			});
+
+			</script>';
+			}
+		}
+		else
+		{
+			echo '<script>
+			
+			swal({
+
+				type: "error",
+				title: "¡El usuario no puede ir vacío o llevar caracteres especiales!",
+				showConfirmButton: true,
+				confirmButtonText: "Cerrar"
+
+			}).then(function(result){
+
+				if(result.value){
+				
+					window.location = "usuarios";
+
+				}
+
+			});
+
+			</script>';
+			}
+		}
+	}
 }
 	
 
